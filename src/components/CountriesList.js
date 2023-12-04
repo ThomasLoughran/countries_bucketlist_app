91c727bbe852e27dfb1a7ce6e50c638b31e8961e
Thomas Loughran <tloughran3@gmail.com>
2023-12-04 14:31:05 +0000

    created skeleton structure for app

---
+++ src/components/CountriesList.js
@@ -0,0 +1,10 @@
+const CountriesList = () => {
+
+    return(
+        <>
+            <h3>This is a list of countries</h3>
+        </>
+    )       
+}
+
+export default CountriesList;
---
+++ src/containers/CountriesContainer.js
@@ -0,0 +1,13 @@
+import CountriesList from "../components/CountriesList";
+
+const CountriesContainer = () => {
+
+    return(
+        <>
+            <h2>This is a countries Container</h2>
+            <CountriesList/>
+        </>
+    )
+}
+
+export default CountriesContainer;
