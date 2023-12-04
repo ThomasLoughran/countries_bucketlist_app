5c92cc40f16d69467065c440be9e6c5690fa67fb
Thomas Loughran <tloughran3@gmail.com>
2023-12-04 16:44:52 +0000

    List of countries is displayed more cleanly and with a flag

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,13 +1,21 @@
 
 const CountriesList = ({countriesList}) => {
 
-    const countryNames = countriesList.map((country) => {
-        return(
-            country.name.common
-        )
-    })
-    console.log(countryNames)
+    
 
+    const Country = (country) => {
+        return(
+            <div>
+                {country.name.common}
+                {country.flag}
+            </div>
+        )
+    } 
+    const countryNames = countriesList.map((country) => {
+            return(
+                Country(country)
+            )
+        })
     return(
         <>
             <h3>This is a list of countries</h3>
